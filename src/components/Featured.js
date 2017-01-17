bad1950da6bf4422b2b9fe409745f3fae7fb2266
Rob DelValle <robdel13@gmail.com>
2017-01-17 15:00:28 -0700

    featured video component added

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -5,13 +5,16 @@ class Featured extends Component {
     render() {
         return (
             <div>
-                <a-assets>
-                    <video id="video" src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/" autoplay="false" loop="false" crossorigin></video>
-                </a-assets>
                 <a-scene id="featuredCanvas" embedded>
+                    <a-assets>
+                        <video id="video" src="/assets/explore360.mp4" autoPlay loop></video>
+                    </a-assets>
                     <a-videosphere src="#video" rotation="0 180 0"></a-videosphere>
 
                 </a-scene>
+                <div className="feat-banner">
+                    <p>Featured Experience</p>
+                </div>
             </div>
         );
     }
