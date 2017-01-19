e6da19102a0f0ee6ee2ef6f7a5ec2db6cad4d570
Rob DelValle <robdel13@gmail.com>
2017-01-18 20:05:50 -0700

    featured page now has featured videos component

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
-import AudioCtrl from './audioController';
-import aframe from 'aframe';
+import FeaturedVideos from './featuredPageVideos';
 
 class Featured extends Component {
     render() {
         return (
-            <div>
-                <a-scene id="featuredCanvas" embedded>
-                    <a-assets>
-                        <video id="video" src="./assets/explore360.mp4" autoPlay loop crossOrigin></video>
-                    </a-assets>
-                    <a-videosphere src="#video" rotation="0 180 0"></a-videosphere>
-
-                </a-scene>
-                <div className="feat-banner">
-                    <p>Featured Experience</p>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-12 col-md-12">
+                        <a-scene id="featuredCanvas" embedded>
+                            <a-assets>
+                                <video id="video" src="./assets/explore360.mp4" crossOrigin></video>
+                            </a-assets>
+                            <a-videosphere src="#video" rotation="0 180 0"></a-videosphere>
+                        </a-scene>
+                        <div className="feat-banner">
+                            <p>Featured Experience</p>
+                        </div>
+                    </div>
                 </div>
-                <AudioCtrl />
+                <div className="row">
+                    <FeaturedVideos />
+                </div>
             </div>
         );
     }
---
+++ src/components/featuredPageVideos.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class FeaturedVideos extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-4 col-md-4">
+                        test
+                    </div>
+                    <div className="col-sm-4 col-md-4">
+                        test
+                    </div>
+                    <div className="col-sm-4 col-md-4">
+                        test
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FeaturedVideos;
