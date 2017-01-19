faf3586a0ace30d97fe6453a0f14808fe6c8c8e3
Rob DelValle <robdel13@gmail.com>
2017-01-19 15:21:35 -0700

    featured branch displays most popular catageory beneath featured video

--- src/components/featuredPageVideos.js
+++ src/components/featuredPageVideos.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 
 class FeaturedVideos extends Component {
+    showFeaturedVideos() {
+        return this.props.videos.map((video) => {
+            return (
+                <div key={video.id} className="col-sm-4 featuredList">
+                    <div>
+                        <a href="#" className="thumbnail"><img src={video.thumbnail} /></a>
+                    </div>
+                    <div className="videoContentBox">
+                        <h3>{video.title}</h3>
+                        <hr />
+                        Username : {video.user_id}
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm-4 col-md-4">
-                        test
+                    <div className="col-sm-12 mostPopularTitle">
+                        <h2>Most Popular</h2>
+                        <hr />
                     </div>
-                    <div className="col-sm-4 col-md-4">
-                        test
-                    </div>
-                    <div className="col-sm-4 col-md-4">
-                        test
-                    </div>
+                </div>
+                <div className="row">
+                    {this.showFeaturedVideos()}
                 </div>
             </div>
         );
     }
 }
 
-export default FeaturedVideos;
+function mapStateToProps(state) {
+    return {
+        videos: state.videos
+    }
+
+}
+
+export default connect(mapStateToProps)(FeaturedVideos);
