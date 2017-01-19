00ac02e982e6658468aee9c75ef711be57a8c558
Rob DelValle <robdel13@gmail.com>
2017-01-19 16:05:05 -0700

    signup page change to fix conflict

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-import signUpAction from '../actions/signUpActions';
+// import signUpAction from '../actions/signUpActions';
 
 class SignUp extends Component {
     constructor( props ) {
