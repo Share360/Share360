76dc0fe81558442cbae1a297eceed235e8a02d4e
Kyle VanDeRiet <kylevdr@gmail.com>
2017-02-02 10:34:53 -0700

    Change navbar link style and favorites page layout

--- db/getFavoritesById.sql
+++ db/getFavoritesById.sql
@@ -1,3 +1,4 @@
-SELECT * FROM public."Favorites"
+SELECT public."Videos".*, public."Users".username FROM public."Favorites"
 JOIN public."Videos" on video_id = public."Videos".id
-WHERE user_id = $1;
+JOIN public."Users" on public."Videos".uploader_id = public."Users".id
+WHERE public."Favorites".user_id = $1;
