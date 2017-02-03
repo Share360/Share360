60be76b137f6c8981480ee9dea862f01b6b876b2
kylecheney <kylejosephcheney@gmail.com>
2017-02-03 00:13:18 -0700

    comment thumbnails

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -9,11 +9,18 @@ export default class Comment extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<Link to={`/profile/${this.props.user_id}`}>{this.props.username}</Link>
-				<p><i>{moment(this.props.time).format("M/D/YYYY h:mm A")}</i></p>
-				<p>{this.props.content}</p>
-				<hr />
+			<div className="row">
+				<div className="col-sm-2 col-md-1">
+					<img src={this.props.profile_url} className="img-circle comment-image" />
+				</div>
+				<div className="col-sm-10 col-md-11">
+					<div>
+						<Link to={`/profile/${this.props.user_id}`}>{this.props.username}</Link>
+						<p><i>{moment(this.props.time).format("M/D/YYYY h:mm A")}</i></p>
+						<p>{this.props.content}</p>
+						<hr />
+					</div>
+				</div>
 			</div>
 		);
 	}
